117638a4598400a468a6c11b52715396969a79e2
Anaïs Eudes <aeudes@takima.fr>
2026-01-30 22:09:31 +0100

    Initial commit

---
+++ src/main/kotlin/main/java/io/takima/teamupback/TeamUpBackApplication.kt
@@ -0,0 +1,11 @@
+package main.java.io.takima.teamupback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamUpBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamUpBackApplication>(*args)
+}
---
+++ src/test/kotlin/main/java/io/takima/teamupback/TeamUpBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package main.java.io.takima.teamupback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamUpBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

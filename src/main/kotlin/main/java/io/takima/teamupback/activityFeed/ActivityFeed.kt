b35887cf8a9c99227d750564d19d357774173dfd
jo-pouradier <jocharbo43@gmail.com>
2026-01-31 02:32:22 +0100

    fix: flyway

--- src/main/kotlin/main/java/io/takima/teamupback/activityFeed/ActivityFeed.kt
+++ src/main/kotlin/main/java/io/takima/teamupback/activityFeed/ActivityFeed.kt
@@ -1,6 +1,7 @@
 package main.java.io.takima.teamupback.activityFeed
 
 import jakarta.persistence.*
+import main.java.io.takima.teamupback.post.Post
 
 @Entity
 @Table(name = "activity_feed")
@@ -8,5 +9,13 @@ class ActivityFeed(
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "activity_feed_id_seq")
     @SequenceGenerator(name = "activity_feed_id_seq", sequenceName = "activity_feed_id_seq", allocationSize = 1)
-    val id: Int? = null
+    val id: Int? = null,
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "activity_feed_post",
+        joinColumns = [JoinColumn(name = "activity_feed_id")],
+        inverseJoinColumns = [JoinColumn(name = "post_id")]
+    )
+    var posts: MutableSet<Post> = mutableSetOf()
 )

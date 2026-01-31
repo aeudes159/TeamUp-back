379e898a550b93efa23773067801d138047e2964
tploix <tploix@takima.fr>
2026-01-31 02:45:50 +0100

    Cors

---
+++ src/main/kotlin/main/java/io/takima/teamupback/common/CorsConfig.kt
@@ -0,0 +1,27 @@
+package main.java.io.takima.teamupback.common
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    @Profile("!prod")  // Active pour tous les profils sauf "prod"
+    fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOriginPattern("*")  // Permet tous les origins en local
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        
+        return CorsFilter(source)
+    }
+}
